3f1e117723c95f20c10c624ffe60bcacd6b48fbe
mrbrunelli <matheus.brunelli@gmail.com>
2022-05-07 15:49:27 -0300

    add salesTotalAmount example

--- src/salesTotalAmount.js
+++ src/salesTotalAmount.js
@@ -3,6 +3,35 @@ const { getClient } = require("./client");
 async function main() {
   const client = await getClient();
   const collection = client.collection("sales");
+
+  const result = await collection
+    .aggregate([
+      {
+        $group: {
+          _id: "$item",
+          totalSaleAmount: {
+            $sum: {
+              $multiply: ["$price", "$quantity"],
+            },
+          },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          item: "$_id",
+          total: {
+            $trunc: ["$totalSaleAmount", 2],
+          },
+        },
+      },
+    ])
+    .toArray();
+
+  return result;
 }
 
-main();
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => process.exit(1));
